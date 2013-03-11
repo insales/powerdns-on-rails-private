b2491cc352408182291b9bbef7be91069daeaa88
Max Melentiev <melentievm@gmail.ru>
2013-03-11 17:18:21 +0400

    fix for dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
 
   def index
-    @latest_domains = Domain.user(current_user).order('created_at DESC').limit(5)
+    @latest_domains = Domain.unscoped.user(current_user).order('created_at DESC').limit(5)
   end
 end
