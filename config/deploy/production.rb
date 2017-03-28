1b7641133980526e102ac3bca8f85c5f60a459f5
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2017-03-28 23:02:12 +0300

    пробуем использовать gemset, чтобы обойти проблему деплоя через shipit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,7 @@
-role :app, 'powerdnsapp3.insales.ru', 'powerdnsapp6.insales.ru'
-role :db,  'powerdnsapp3.insales.ru', primary: true
+# role :app, 'powerdnsapp3.insales.ru', 'powerdnsapp6.insales.ru'
+# role :db,  'powerdnsapp3.insales.ru', primary: true
+
+role :app, 'powerdnsapp6.insales.ru'
 
 namespace :deploy do
   task :restart do
