ae1ea6a334576bd6069ac081341a294c488598ab
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2017-03-28 23:30:37 +0300

    возвращаем деплой на powerdnsapp3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,5 @@
-# role :app, 'powerdnsapp3.insales.ru', 'powerdnsapp6.insales.ru'
-# role :db,  'powerdnsapp3.insales.ru', primary: true
-
-role :app, 'powerdnsapp6.insales.ru'
+role :app, 'powerdnsapp3.insales.ru', 'powerdnsapp6.insales.ru'
+role :db,  'powerdnsapp3.insales.ru', primary: true
 
 namespace :deploy do
   task :restart do
