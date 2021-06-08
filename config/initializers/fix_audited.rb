2a3c84bcdaefe5a837634736575b3d29c282e961
Vasily Fedoseyev <vasilyfedoseyev@gmail.com>
2021-06-09 02:47:01 +0300

    Fix audited

--- config/initializers/fix_audited.rb
+++ config/initializers/fix_audited.rb
@@ -1,7 +1,4 @@
 Audit = Audited::Adapters::ActiveRecord::Audit
-#TODO: potential vulnarability
-Audit.attr_accessible :username, :user, :version, :auditable
-
 
 # пока не тащим миграцию из audited:upgrade, т.к. таблица там большая
 class Audit
