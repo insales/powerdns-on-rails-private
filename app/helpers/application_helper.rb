9d8a69b8b74c0b58c156966847e293779c6f6239
Vasily Fedoseyev <vasilyfedoseyev@gmail.com>
2021-06-09 03:00:31 +0300

    Remove link_to_function warnings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,4 +44,13 @@ module ApplicationHelper
     image_tag( image , :id => "help-icn-#{dom_id}", :class => 'help-icn', "data-help" => dom_id )
   end
 
+  def link_to_function(name, function, html_options={})
+    # forward-port from rails 4
+    # TODO: переделать UI на Unobtrusive JavaScript
+
+    onclick = "#{"#{html_options[:onclick]}; " if html_options[:onclick]}#{function}; return false;"
+    href = html_options[:href] || '#'
+
+    content_tag(:a, name, html_options.merge(:href => href, :onclick => onclick))
+  end
 end
